3ad046af064abde385183c1640e4e5ce49078908
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2023-03-15 08:41:48 +0200

    create splash screen for the app

---
+++ lib/config/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  static HexColor primaryColor = HexColor('#457b9d');
+  static HexColor white = HexColor('#ffffff');
+  static HexColor grey = HexColor('#6c757d');
+  static HexColor black = HexColor('#2f3e46');
+}
---
+++ lib/screen/splash.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
+import 'package:get/route_manager.dart';
+import '../config/colors.dart';
+import 'package:get/get.dart';
+import './Login.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Get.to(const Login());
+    });
+    return Scaffold(
+      backgroundColor: GlobalColors.primaryColor,
+      body: Center(
+        child: Text(
+          'Amategeko',
+          style: TextStyle(
+              color: GlobalColors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
